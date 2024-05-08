044850b028c493b64d668133296f51277ebec9b8
FOUDA-TANGUY <blondelle.fouda@facsciences-uy1.cm>
2024-05-08 10:06:53 +0100

    organisation

--- Exo5 (Calcul matriciel)/src/matrix.c
+++ Exo5 (Calcul matriciel)/src/matrix.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "matrix.h"
-#include "type.h"
+#include <type.h>
+#include <matrix.h>
 
 //------------ Fonction pour calculer la transposee d'une matrice ------------------------
 matrice* Transposer(matrice mat){
